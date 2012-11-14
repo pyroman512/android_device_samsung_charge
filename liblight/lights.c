64676358e132eaf6985a3238481bb0a74dc2e790
sbrissen <sbrissen@hotmail.com>
2012-11-14 13:55:42 -0500

    fix button backlights

--- liblight/lights.c
+++ liblight/lights.c
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-//#define LOG_NDEBUG 0
+#define LOG_NDEBUG 0
 #define LOG_TAG "lights"
 #include <cutils/log.h>
 #include <stdint.h>
@@ -33,6 +33,12 @@ static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
 char const *const LCD_FILE = "/sys/class/backlight/s5p_bl/brightness";
 char const *const LED_FILE = "/sys/class/key/key/brightness";
+
+void init_globals(void)
+{
+    // init the mutex
+    pthread_mutex_init(&g_lock, NULL);
+}
 
 static int write_int(char const *path, int value)
 {
@@ -86,12 +92,31 @@ static int set_light_backlight(struct light_device_t *dev,
 {
 	int err = 0;
 	int brightness = rgb_to_brightness(state);
+	int ledbrightness = !!(state->color & 0x00ffffff);
 
 	pthread_mutex_lock(&g_lock);
 	err = write_int(LCD_FILE, brightness);
 
+	err = write_int(LED_FILE, ledbrightness);
+
 	pthread_mutex_unlock(&g_lock);
 	return err;
+}
+
+static int
+set_light_buttons(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    int brightness = !!(state->color & 0x00ffffff);
+    int err = 0;
+
+    pthread_mutex_lock(&g_lock);
+    ALOGD("set_light_button on=%u\n", brightness);
+    err = write_int(LED_FILE, brightness);
+    pthread_mutex_unlock(&g_lock);
+
+    return err;
+
 }
 
 static int close_lights(struct light_device_t *dev)
@@ -115,6 +140,8 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 		set_light = set_light_backlight;
 	else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
 		set_light = set_light_notifications;
+	else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+		set_light = set_light_buttons;
 	else
 		return -EINVAL;
 
